ef59ad1455438a0d470edae23ee329b8c1c50037
G <188799@jnu.ac.kr>
2025-09-11 00:19:33 +0900

    Update code

--- chat01.py
+++ chat01.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from openai import OpenAI
-client = OpenAI(api_key="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")
+client = OpenAI()
 
 response = client.responses.create(
     model="gpt-5",
